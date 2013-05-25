84617703b3b0ebb0a5847a06e65bc68f17b93f99
Ilya Arkhanhelsky <dm@dm>
2013-05-25 08:39:36 +0300

    Пофиксил "ту самую функцию". Тестовый скрипт про это приглагается.

--- common/src/task_function.m
+++ common/src/task_function.m
@@ -2,22 +2,21 @@
 % Аналогично индикатору области из area.m создадим кусочно непрерывную функцию по таким же правилам.
 
 function [ f ] = task_function(bar_heights, bar_sizes, x, y)
-    function [ y ] = task_f(x)
+    function [ v ] = task_f(u)
             % Смещаем в 0, 0
             u = u - x;
-
 
             j = 1;
             while (j <= length(bar_sizes)) && (u > bar_sizes(j))
                 j = j + 1;
             end
 
-            if (j > length(bar_sizes))
-                y = 0;
+            if (j > length(bar_sizes) || (u < 0))
+                v = 0;
             else
-                y =  bar_heights(j);
+                v = y + bar_heights(j);
             end
         end
 
-        area = @task_f;
+        f = @task_f;
 end
