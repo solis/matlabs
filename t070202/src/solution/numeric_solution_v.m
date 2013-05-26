f5bbf56463b2880b8ba7caf21523e1da78bf8e17
Ilya Arkhanhelsky <dm@dm>
2013-05-26 10:16:23 +0300

    Финальный фикс, большая ошибка только около нуля, но я полагаю, что это из-за малости значений.

--- t070202/src/solution/numeric_solution_v.m
+++ t070202/src/solution/numeric_solution_v.m
@@ -3,10 +3,9 @@ function [ u ] = numeric_solution_v( x, t )
         u = (phi( x + a*t ) + phi( x - a*t )) / 2 + (1 / (2*a)) * quad(@(x) Psi(x), x - a*t, x + a*t);
     else
         if ( t > x / a )
-            u = (phi( x + a*t ) - phi( x - a*t )) / 2 + (1 / (2*a)) * quad(@(x) Psi(x), a*t - x, x + a*t);
+            u = (phi( x + a*t ) - phi( a*t - x )) / 2 + (1 / (2*a)) * quad(@(x) Psi(x), a*t - x, x + a*t);
         else
-            % Доопределим функцию в точке t = u, занчением  в точке (x + 0.0001, t). Это заодно не будет вводить quad в замешательство, когда промежуток интегрирования становится длинны 0
-            u =  numeric_solution_v(x + 0.0001, t);
+            u = numeric_solution_v(x+0.00000001, t);
         end
     end
 end
