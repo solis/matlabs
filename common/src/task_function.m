bbb027f27b36bca8519d794beef5c7104a3e59c3
Ilya Arkhanhelsky <dm@dm>
2013-05-27 23:59:13 +0300

    task_f теперь уж точно правильно принимает вектор.

--- common/src/task_function.m
+++ common/src/task_function.m
@@ -3,20 +3,21 @@
 
 function [ f ] = task_function(bar_heights, bar_sizes, x, y)
     function [ v ] = task_f(u)
+        v = zeros(1,length(u));
+        for k = 1:length(u)
             % Смещаем в 0, 0
-            u = u - x;
+            u(k) = u(k) - x;
 
             j = 1;
-            while (j <= length(bar_sizes)) & (u > bar_sizes(j))
+            while (j <= length(bar_sizes)) && (u(k) > bar_sizes(j))
                 j = j + 1;
             end
 
-            if (j > length(bar_sizes) | (u < 0))
-                v = 0;
-            else
-                v = y + bar_heights(j);
+            if ~(j > length(bar_sizes) || (u(k) < 0))
+                v(k) = y + bar_heights(j);
             end
         end
+    end
 
-        f = @task_f;
+    f = @task_f;
 end
--- common/test/test_function.m
+++ common/test/test_function.m
@@ -5,10 +5,9 @@ bars = area_description(702, 1, 0.3);
 
 task_f = task_function(bars, [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], 0, 0);
 
-for i = 1:length(X)
-    Y(i) = task_f(X(i));
-end
+Y = task_f(X);
 
+task_f([0, 0.5, 0.3, 0.4, 0.05])
 
 plot(X, Y);
 axis([min(X) max(X) min(Y) - 0.2 max(Y) + 0.2])
