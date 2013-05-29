40877e2807015c6303c97cccbb7aa8d1f732e066
Ilya Arkhanhelsky <dm@dm>
2013-05-29 15:35:46 +0300

    After merge fix ( solis/master -> iarkhanhelsky/master)

--- t070202/src/solution/analytic_solution.m
+++ t070202/src/solution/analytic_solution.m
@@ -1,19 +1,19 @@
 function [ u ] = analytic_solution( x, t )
     function [ v ] = v(x, t)
-        if (t > x / a)
-            v = (phi( x + a*t ) + phi( x - a*t )) / 2 + (2*a*t * (a.^2 * t.^2 + 3*x.^2 - 2*x + 3)) / (2*a);
+        if (t < x / a)
+            v = (phi( x + a*t ) + phi( x - a*t)) / 2 + (2*a*t * (a.^2 * t.^2 + 3*x.^2 - 2*x + 3)) / (2*a);
         else
-            v = (phi( x + a*t ) - phi( x - a*t )) / 2 +  (2*x * (3*(a.^2) * (t.^2) + 2*a*t + x.^2 + 3)) / (2*a);
+            v = (phi( x + a*t ) - phi( a*t - x)) / 2 +  (2*x * (3*(a.^2) * (t.^2) + 2*a*t + x.^2 + 3)) / (2*a);
         end
     end
 
     function [ w ] = w(x, t)
         if (t < x / a)
             w = (-2*a*t.^3 - 14*a*t.^2 + 48 * a * t.^2 * x)/ (2*a) ;
-        else            
+        else
             i1 = -(6*x * ((x - a*t).^2))/(a.^2) + (28*x*(x - a*t)/ a) +  48*x*( a*t.^2 - x.^2/a);
-            i2 = (4*x.^3 - 6*a*t*x.^2)/a - (14*x.^2) / a + (48*x.^3) / a;                        
-            w = ( i1 + i2 ) / (2*a);                        
+            i2 = (4*x.^3 - 6*a*t*x.^2)/a - (14*x.^2) / a + (48*x.^3) / a;
+            w = ( i1 + i2 ) / (2*a);
         end
     end
 
@@ -25,6 +25,6 @@ function [ u ] = analytic_solution( x, t )
         end
     end
 
-    u = z(x, t) + v(x, t) + w(x, t);
+    u = w(x,t) + v(x, t) + z(x, t);
 end
 
