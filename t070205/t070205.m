b7eb50c460558fa51dd52f61624b836f066d672f
Ilya Arkhanhelsky <dm@dm>
2013-06-01 15:41:47 +0300

    Добавил визуализацию и придумал красивую функцию. Решает только не правильно.

--- t070205/t070205.m
+++ t070205/t070205.m
@@ -1,20 +1,62 @@
-load('t070205/solution.mat');
+function [] = t070205
 
-X = 0:0.01:1;
-Y = 0:0.01:1;
+    load('t070205/pde_solution.mat');
 
-UXY = tri2grid(p, t, u, X,Y);
+    X = 0:0.01:1;
+    Y = 0:0.01:1;
 
-real = @(x, t) sin(x) * cos(t);
-subplot(2, 1, 1)
-m = mesh(X, Y, UXY);
+    UXY = tri2grid(p, t, u, X, Y);
 
-subplot(2, 1, 2)
+    [X, Y] = meshgrid(X, Y);
+    AXY = cos(20 .* (X - 1) .* (Y - 1));
+    AXY(isnan(UXY)) = NaN;
 
-Z = [];
-for i = 1:length(X)
-    for j = 1:length(Y)
-        Z(i, j) = real(X(i), Y(j));
-    end
+    AERR = abs(UXY - AXY);
+    RERR = AERR ./ UXY;
+
+    %%Визуализация
+
+    subplot(2, 1, 1)
+    surf(X, Y, UXY);
+    title('PDETool solution')
+    shading flat;
+    view(-128, 46);
+
+    subplot(2, 1, 2)
+    surf(X, Y, AXY);
+    title('Exact solution');
+    shading flat;
+    view(-128, 46);
+
+    figure;
+
+    subplot(2, 1, 1);
+    [C, h] = contourf(X, Y, UXY);
+    title('PDETool solution');
+    clabel(C, h);
+    colorbar('EastOutside');
+
+    subplot(2, 1, 2);
+    [C, h] = contourf(X, Y, AXY);
+    title('Exact solution');
+    clabel(C, h);
+    colorbar('EastOutside');
+
+
+    figure;
+
+    subplot(2, 1, 1);
+    [C, h] = contourf(X, Y, AERR);
+    title('Absolute error');
+    clabel(C, h);
+    colorbar('EastOutside');
+
+
+    subplot(2, 1, 2);
+    [C, h] = contourf(X, Y, RERR);
+    title('Relative error');
+    clabel(C, h);
+    colorbar('EastOutside');
+
+    clear p; clear u; clear t; clear e;
 end
-surf(X, Y, Z);
