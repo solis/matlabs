ce392c6a1a66e56aeaf0497e6d4ccf818c00d0dd
Ilya Arkhanhelsky <dm@dm>
2013-06-03 12:07:04 +0300

    Analytic solution fixes

--- t070202/src/solution/numeric_solution_w.m
+++ t070202/src/solution/numeric_solution_w.m
@@ -1,10 +1,16 @@
 function [ u ] = numeric_solution_w( x, t )
-    if ( t <= x / a )
-        u = dblquad( @(z, p) f(z, p) .* (z >= x - a*(t - p)) .* (z <= x + a*(t - p)), 0, x + a*t, 0, t) / (2*a);
+    if ( t < x / a )
+        u = dblquad( @(z, p)  f(z, p) .* (z >= x - a*(t - p)) .* (z <= x + a*(t - p)), 0, x + a*t, 0, t) / (2*a);
     else
-        i1 = dblquad( @(z, p) f(z, p) .* (z >= a*(t - p) - x) .* (z <= x + a*(t - p)), 0, x + a*t, 0, t - x / a);
-        i2 = dblquad( @(z, p) f(z, p) .* (z >= x - a*(t - p)) .* (z <= x + a*(t - p)), 0, 2*x, t - x / a, t);
-        u = (i1 + i2) / (2*a);
+        if (t > x / a)
+            i1 = 0;
+            i2 = 0;
+            i1 = dblquad( @(z, p) f(z, p) .* (z >= a*(t - p) - x) .* (z <= x + a*(t - p)), 0, x + a*t, 0, t - x / a);
+            i2 = dblquad( @(z, p) f(z, p) .* (z >= x - a*(t - p)) .* (z <= x + a*(t - p)), 0, 2*x, t - x / a, t);
+            u = (i1 + i2) / (2*a);
+            else
+                u = (numeric_solution_w(x + 0.0001, t) + numeric_solution_w(x - 0.0001, t)) /2;
+        end
     end
 end
 
