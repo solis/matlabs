ad70b4114fde368850d68387a09056d9fbb0caa3
Ilya Arkhanhelsky <dm@dm>
2013-06-08 15:07:26 +0300

    Structure

--- t070203/src/plot_contour_err.m
+++ t070203/src/plot_contour_err.m
@@ -1,7 +1,7 @@
-function [] = plot_err(X, T, U, U_a)
+function [] = plot_contour_err(X, T, U, U_a)
     figure;
     subplot(2, 1, 1)
-        [C, h] = contourf(X, T, U);
+        [C, h] = contourf(X, T, U, 20);
         shading flat;
         title('Absolute error');
         xlabel('x');
@@ -11,7 +11,7 @@ function [] = plot_err(X, T, U, U_a)
 
 
     subplot(2, 1, 2)
-        [C, h] = contourf(X, T, U_a, [0.0001, 0.0005, 0.001, 0.005, 0.007, 0.009, 0.01, 0.05, 0.1, 0.3, 0.5, 0.8, 1 ]);
+        [C, h] = contourf(X, T, U_a, 20);
         shading flat;
         title('Relative error');
         xlabel('x');
