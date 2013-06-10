64185b28416baa49a31d90bfe6778b9b9cf008ef
Ilya Arkhanhelsky <dm@dm>
2013-06-10 23:32:54 +0300

    Final fix. Close ALL.

--- t070203/src/plot_contour.m
+++ t070203/src/plot_contour.m
@@ -1,14 +1,17 @@
 function [] = plot_contour(X, T, U, U_a)
     figure;
-    subplot(2, 1, 1), contourf(X, T, U);
+    subplot(2, 1, 1), [C1,h1] = contour(X, T, U, -0.5:0.05:0);
+    clabel(C1, h1, 'LabelSpacing', 300);
     title('Numeric solution contour');
     colorbar('location', 'EastOutside')
     xlabel('x');
     ylabel('t');
 
-    subplot(2, 1, 2), contourf(X, T, U_a);
+    subplot(2, 1, 2), [C2,h2] = contour(X, T, U_a,-0.5:0.05:0);
+    clabel(C2, h2, 'LabelSpacing', 300);
     title('Analytic solution contour', 'FontName', 'Arial Cyr');
     colorbar('location', 'EastOutside')
     xlabel('x');
     ylabel('t');
+
 end
